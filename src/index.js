a4b8d9345e29d8cc5af900a233706665b6c495ff
Tim Zeng <zengtiantian1122@gmail.com>
2017-10-23 15:09:07 -0700

    add components folder and css file folder

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const title = 'My Minimal React Webpack Babel Setup';
+import App from './components/app';
 
 ReactDOM.render(
-  <div>{title}</div>,
+  <App />,
   document.getElementById('app')
 );
-
-module.hot.accept();
